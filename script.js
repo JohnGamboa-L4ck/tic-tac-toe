b31792e2519d664d1729f4088ab903a2a5ac42b5
John Gamboa <jayjeex@gmail.com>
2021-06-23 17:49:24 +0800

    commit before experimenting

--- script.js
+++ script.js
@@ -1,5 +1,35 @@
 'use strict';
 
+const display = (function(){
+
+    let sampleOne = 1;
+    const sampleTwo = '2';
+    let sampleThree = { name: 3};
+
+    const home = document.querySelector('#home');
+    const setting = document.querySelector('#setting');
+
+    const hideHome = () => home.classList.add('hide');
+    const showHome = () => home.classList.remove('hide');
+    const showSetting = () => {
+        hideHome();
+        setting.classList.add('show');
+    };
+
+    return { 
+        hideHome, showHome, showSetting
+    }
+})();
+
+
+
+
+const game = (function(){
+})();
+
+const playButton = document.querySelector('#playButton');
+playButton.addEventListener('click', display.showSetting);
+
 // start the game
 // restart the game
 
